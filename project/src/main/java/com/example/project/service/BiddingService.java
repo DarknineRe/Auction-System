9d76cb4987bce32b503e7082b8997e7b5de09cfe
zeemongteng <zeemongteng@gmail.com>
2026-09-23 06:13:09 +0700

    create: BiddingService as interface

---
+++ project/src/main/java/com/example/project/service/BiddingService.java
@@ -0,0 +1,13 @@
+package com.example.project.service;
+
+import java.util.List;
+
+import com.example.project.model.Bidding;
+
+public interface BiddingService {
+    Bidding createBidding(Long artworkID, Long ownerID, Double startingPrice,java.time.LocalDate startDate, java.time.LocalDate endDate);
+
+    Bidding getBiddingById(Long biddingID);
+
+    List<Bidding> getAllBiddings();
+}
