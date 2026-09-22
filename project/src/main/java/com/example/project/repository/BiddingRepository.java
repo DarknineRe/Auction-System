c31f6a8310d41678cb915ff015a7ce1e580b3ad3
zeemongteng <zeemongteng@gmail.com>
2026-09-23 05:46:16 +0700

    fix: changed repo's class to interface

--- project/src/main/java/com/example/project/repository/BiddingRepository.java
+++ project/src/main/java/com/example/project/repository/BiddingRepository.java
@@ -1,13 +1,15 @@
 package com.example.project.repository;
 
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
-import com.example.project.model.*;;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.project.model.Bidding;;
 
 
 @Repository
-public class BiddingRepository extends JpaRepository<Bidding, Long> {
+public interface BiddingRepository extends JpaRepository<Bidding, Long> {
     List<Bidding> findByArtwork_ArtworkID(Long artworkID);
     List<Bidding> findByOwner_UserID(Long userID);
 
